0c172b5add2c803b4c4829897dca93553d198520
Abin P S <abinps23@gmail.com>
2024-05-14 18:08:53 +0530

    Authentication with Biometrics if the device has support example app created.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +9,69 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool _isAuthenticated = false;
+  final LocalAuthentication _auth = LocalAuthentication();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: _buildUI(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _authenticate,
+        child: Icon(_isAuthenticated ? Icons.lock : Icons.lock_open),
+      ),
+    );
+  }
+
+  void _authenticate() async {
+    if (!_isAuthenticated) {
+      try {
+        //checking is the device has biometrics authentication support.
+        final bool hasBiometricsAuthentications = await _auth.canCheckBiometrics;
+        //if it has then authenticate
+        if (hasBiometricsAuthentications) {
+          final bool didAuthenticate = await _auth.authenticate(
+              localizedReason: "Please authenticate to show account balance.",
+              options: const AuthenticationOptions(
+                biometricOnly: false, //if it is true, patterns verifcation not allowed. biometrics only - Face ID or FingerPrint
+              ));
+
+          setState(() {
+            _isAuthenticated = didAuthenticate;
+          });
+        }
+      } catch (e) {
+        print(e);
+      }
+    } else {
+      setState(() {
+        _isAuthenticated = !_isAuthenticated;
+      });
+    }
+  }
+
+  Widget _buildUI() {
+    const TextStyle defaultTextStyle = TextStyle(fontSize: 20, fontWeight: FontWeight.w500);
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text(
+            "Account Balance",
+            style: defaultTextStyle,
+          ),
+          if (_isAuthenticated)
+            const Text(
+              "\$ 87,500",
+              style: defaultTextStyle,
+            ),
+          if (!_isAuthenticated)
+            const Text(
+              "******",
+              style: defaultTextStyle,
+            )
+        ],
+      ),
+    );
   }
 }
